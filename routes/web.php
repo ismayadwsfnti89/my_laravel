e6e1abf80da8894348b2bbf6104ed279a08f8acd
ismayadwsfnti89 <ismayadwsfnti89@gmail.com>
2026-04-07 12:39:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,15 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
-Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');
-Route::get('/products', [ProductController::class, 'product'])->name('product.product');
-Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/products/store', [ProductController::class, 'store'])->name('product.store');
+Route::get('/category', [CategoryController::class, 'index'])->name('admin.category.index');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('admin.category.create');
+Route::post('/category/store', [CategoryController::class, 'store'])->name('admin.category.store');
+Route::get('/products', [ProductsController::class, 'index'])->name('admin.products.index');
+Route::get('/products/create', [ProductsController::class, 'create'])->name('admin.products.create');
+Route::post('/products/store', [ProductsController::class, 'store'])->name('admin.products.store');
+Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->name('admin.category.destroy');
+Route::delete('/products/{id}', [ProductsController::class, 'destroy'])->name('admin.products.destroy');
+Route::get('/category/{id}', [CategoryController::class, 'edit'])->name('admin.category.edit');
+Route::put('/category/{id}', [CategoryController::class, 'update'])->name('admin.category.update');
+Route::get('/products/{id}/edit', [ProductsController::class, 'edit'])->name('admin.products.edit');
+Route::put('/products/{id}', [ProductsController::class, 'update'])->name('admin.products.update');
